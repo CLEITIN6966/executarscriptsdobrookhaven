15d97b4c60e839832611d65bc23d5fe7ec39881c
CLEITIN6966 <cleitinholeiti@gmail.com>
2024-08-11 17:07:02 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,8 +1,8 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source'))()"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/fly.lua"))()
-local notificação = loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/notificação.lua"))();
-local Notify = notificação.Notify;
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
 Notify({
 Description = "by CLEITI6966";
 Title = "load success";
