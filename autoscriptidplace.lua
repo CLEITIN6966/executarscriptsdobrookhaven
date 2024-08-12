c83a4f5b31f72c3582d441481d5aa0fc14292a1d
CLEITIN6966 <cleitinholeiti@gmail.com>
2024-08-12 11:57:53 -0300

    Update and rename autoscriptidplace to autoscriptidplace.lua

--- autoscriptidplace.lua
+++ autoscriptidplace.lua
@@ -5,7 +5,7 @@ if mapID == 4924922222 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/script.lua"))()
 elseif mapID == 2753915549 then
     -- Execute the script for the second map
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/blox%20fruits"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/blox%20fruits.lua"))()
 elseif mapID == 4483381587 then
     -- Execute the script for the third map
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/executarscriptsdobrookhaven/main/script.lua"))()
